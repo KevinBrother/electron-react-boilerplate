4ef9b76f9fd0cc58253df558418c5446f4da7fbe
caojunjie <1301239018@qq.com>
2022-11-21 16:34:31 +0800

    feat(not-isloation) 增加非隔离时的文件

---
+++ src/main/preload-not-isloation.ts
@@ -0,0 +1,33 @@
+import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
+
+export type Channels = 'ipc-example';
+
+window.electron = {
+  ipcRenderer: {
+    sendMessage(channel: Channels, args: unknown[]) {
+      ipcRenderer.send(channel, args);
+    },
+    on(channel: Channels, func: (...args: unknown[]) => void) {
+      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
+        func(...args);
+      ipcRenderer.on(channel, subscription);
+
+      return () => ipcRenderer.removeListener(channel, subscription);
+    },
+
+    once(channel: Channels, func: (...args: unknown[]) => void) {
+      ipcRenderer.once(channel, (_event, ...args) => func(...args));
+    },
+  },
+};
+
+window.versions = {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+};
+
+window.process = {
+  pid: () => process.pid,
+  ppid: () => process.ppid,
+};
